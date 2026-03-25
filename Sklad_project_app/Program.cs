11a9d135e726ae6aeefb2ba51d644564c6d5d826
MullinArseniy <arsmullin2007@gmail.com>
2026-03-25 19:43:17 +0300

    Дополнение хэширования

--- Sklad_project_app/Program.cs
+++ Sklad_project_app/Program.cs
@@ -5,6 +5,7 @@ namespace Sklad_project_2
         [STAThread]
         static void Main()
         {
+            Application.SetHighDpiMode(HighDpiMode.SystemAware);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new LoginForm());
